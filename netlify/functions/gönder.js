1010b843e0c76caf520b45987dfcf3eae61983eb
ezel39 <cemre333elif@gmail.com>
2025-07-13 15:26:36 +0300

    gönder.js

---
+++ netlify/functions/gönder.js
@@ -0,0 +1,27 @@
+const { Client } = require("pg");
+
+exports.handler = async function (event) {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Sadece POST kabul edilir." };
+  }
+
+  const { ad, email, mesaj } = JSON.parse(event.body);
+
+  const client = new Client({
+    connectionString: process.env.NEON_DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  });
+
+  try {
+    await client.connect();
+    await client.query(
+      "INSERT INTO iletisim_formu (ad, email, mesaj) VALUES ($1, $2, $3)",
+      [ad, email, mesaj]
+    );
+    await client.end();
+
+    return { statusCode: 200, body: "Veri kaydedildi." };
+  } catch (err) {
+    return { statusCode: 500, body: "Hata: " + err.message };
+  }
+};

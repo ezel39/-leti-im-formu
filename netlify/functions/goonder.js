5a3cf4f25ae86ac55de3bf1b0cced74b3237d9d3
ezel39 <cemre333elif@gmail.com>
2025-07-13 16:21:46 +0300

    goonder.js

--- netlify/functions/goonder.js
+++ netlify/functions/goonder.js
@@ -1,1 +1,35 @@
+const { Pool } = require('pg');
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: 'Sadece POST isteği destekleniyor.',
+    };
+  }
+
+  try {
+    const data = JSON.parse(event.body);
+    const { ad, email, mesaj } = data;
+
+    const query = 'INSERT INTO iletisim_formu (ad, email, mesaj) VALUES ($1, $2, $3)';
+    await pool.query(query, [ad, email, mesaj]);
+
+    return {
+      statusCode: 200,
+      body: 'Mesaj başarıyla kaydedildi!',
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: 'Hata oluştu: ' + error.message,
+    };
+  }
+};
